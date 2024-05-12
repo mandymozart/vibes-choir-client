048764e4a03916045238d3ce58b3afe2cddf1ff7
Mandy Mozart <mandymozart@viennastruggle.com>
2024-05-12 23:47:27 +0200

    ready to try

--- example.js
+++ example.js
@@ -1,8 +1,8 @@
-// Define a getResponse function exported by the module
+// Define a getResponse function exported by the module Plugin
 exports.getExample = async (value) => {
-    // Set up an initial response array; this can also handle multiple responses to one message
-    let response = [];
+  // Set up an initial response array; this can also handle multiple responses to one message
+  let response = [];
 
-    // Return the response array for the server to use
-    return response;
-}
+  // Return the response array for the server to use
+  return response;
+};
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require('express');
 const app = express();
 
 // Initialize a basic HTTP server to be used by Socket.IO
+// TODO: RUN HTTPS for cross domain https://akshitb.medium.com/how-to-run-https-on-localhost-a-step-by-step-guide-c61fde893771
 const http = require('http');
 const server = http.createServer(app);
 
@@ -18,40 +19,40 @@ app.use(express.static('public'));
 
 // Wrap data in carrier message
 function makeMessage(socket, data) {
-    return {clientId:socket.id,data:data}
+  return { clientId: socket.id, data: data };
 }
 
 // Have Socket.IO listen for socket connections
 io.on('connection', async (socket) => {
-    console.log('Connection made');
-    socket.on('disconnect', () => {
-        console.log('Disconnected client');
-        socket.broadcast.emit('disconnected', {});
-    });
+  console.log('Connection made');
+  socket.on('disconnect', () => {
+    console.log('Disconnected client');
+    socket.broadcast.emit('disconnected', {});
+  });
 
-    socket.on('host',(options)=>{
-        console.log('User hosted session ', options)
-        socket.broadcast.emit('host', makeMessage(socket,options));
-    })
+  socket.on('host', (options) => {
+    console.log('User hosted session ', options);
+    socket.broadcast.emit('host', makeMessage(socket, options));
+  });
 
-    socket.on('join',(group)=>{
-        console.log('User joined group ' + group)
-        socket.broadcast.emit('join', makeMessage(socket, options));
-    })
+  socket.on('join', (group) => {
+    console.log('User joined group ' + group);
+    socket.broadcast.emit('join', makeMessage(socket, options));
+  });
 
-    socket.on('switch',(group)=>{
-        console.log('User switched to group ' + group)
-        socket.broadcast.emit('switch', makeMessage(socket, group));
-    })
+  socket.on('switch', (group) => {
+    console.log('User switched to group ' + group);
+    socket.broadcast.emit('switch', makeMessage(socket, group));
+  });
 
-    socket.on('midi_message', async (midimessage) => {
-        console.log('Received MIDI message:', midimessage);
-        // Forward the MIDI message to all connected clients except the sender
-        socket.broadcast.emit('midi_message', makeMessage(socket,midimessage));
-    });
+  socket.on('midi_message', async (midimessage) => {
+    console.log('Received MIDI message:', midimessage);
+    // Forward the MIDI message to all connected clients except the sender
+    socket.broadcast.emit('midi_message', makeMessage(socket, midimessage));
+  });
 });
 
 // Start up the HTTP server on the given port
 server.listen(PORT, () => {
-    console.log('Server running on port ' + PORT);
+  console.log('Server running on port ' + PORT);
 });
