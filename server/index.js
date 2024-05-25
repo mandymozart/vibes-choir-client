5b7adf3d36ee1b450b7732c3bbf69ff26f0f927a
Mandy Mozart <mandymozart@viennastruggle.com>
2024-05-25 21:43:49 +0200

    fix ports and env

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const path = require('path');
+require('dotenv').config();
 // Define a PORT variable for the server
-const PORT = 3000;
-const HOST = '127.0.0.1';
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '127.0.0.1';
 const ALLOW_ORIGIN = '*';
 // const ALLOW_ORIGIN = "https://viennastruggle.com"
 
